ec7d7c3ebbae9d090daa52cc04279b0fc8282902
goelabhishek694 <barnwalsuman41@gmail.com>
2022-02-12 21:12:57 +0530

    js variable

---
+++ index.js
@@ -0,0 +1,22 @@
+var a;
+a=10;
+console.log(typeof a);
+console.log(a);
+a="hello";
+console.log(typeof a);
+console.log(a);
+a=true;
+console.log(typeof a);
+console.log(a);
+a=null;
+console.log(typeof a);
+console.log(a);
+//String 
+//" ",'',``,
+//Double quotes,Single Quotes, backtick
+var str='a';
+console.log(typeof str);
+str="How you doin?"
+console.log(str);
+
+
