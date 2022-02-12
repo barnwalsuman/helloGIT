5b04801d6aec71a51ef335fbf37644b6273a175b
goelabhishek694 <barnwalsuman41@gmail.com>
2022-02-12 23:08:04 +0530

    let keyword

--- index.js
+++ index.js
@@ -22,10 +22,21 @@ var t=true;
 var f=false;
 console.log(t);
 console.log(f);
+// backtick concept
+//working procedure
 var b=`hi everyone how is going everything fine naa.
 I hope so. `
 console.log(b);
 var num=500;
 console.log(`Half the value of ${num} is a ${num/2}`);
+
+//let keyword 
+//stsrted
+let l=100;
+console.log(l);
+//let l=200;//SyntaxError: Identifier 'l' has already been declared but in case of var it will not give any error
+//console.log(l);
+l=300;
+console.log(l);
 
 
